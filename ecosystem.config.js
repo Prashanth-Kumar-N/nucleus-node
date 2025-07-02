df7e8f9e2b3ef4dda7e68377b9c087291448f621
Prashanth Kumar <70272376+Prashanth-Kumar-N@users.noreply.github.com>
2025-07-02 10:08:28 +0530

    Update ecosystem.config.js to use port 443

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
         AWS_S3_BUCKETNAME: "sunny-nucleus",
         AWS_S3_FILES_FOLDER: "Files",
         MAX_FILE_SIZE: 200,
-        PORT: 80,
+        PORT: 443,
         FILES_PRESIGNED_URL_EXPIRY: 3600,
       },
       env_development: {
